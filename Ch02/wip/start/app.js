13542d1fef7cb676d61808394fc93762a1cc40a5
Deep Patel <dpate71@uic.edu>
2018-04-28 22:06:32 +0000

    Added state to ProductCustomizer

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -3,39 +3,49 @@
   
   function SizeSelector(props) {
     function sizeOptions() {
-      return window.Inventory.allSizes.map(function(num) {
+      return props.sizes.map(function(num) {
         return (
-            <option value={num} key={num}>{num}</option>
-          );
+          <option value={num} key={num}>{num}</option>
+        );
       });
     }
     
     return (
-        <div className="field-group">
-          <label htmlFor="size-options">Size:</label>
-          <select defaultValue={props.size} name="sizeOptions" id="size-options">
-            	{sizeOptions()}
-          </select>
-        </div>
-      );
+      <div className="field-group">
+        <label htmlFor="size-options">Size:</label>
+        <select defaultValue={props.size} name="sizeOptions" id="size-options">
+          	{sizeOptions()}
+        </select>
+      </div>
+    );
   }
   
   function ProductImage(props) {
     return <img src={`../../../assets/${props.color}.jpg`} alt="Product image"/>;
   }
   
-  function ProductCustomizer(props) {
-    return (
-      <div className="customizer">
-        <div className="product-image">
-          <ProductImage color="green" />
+  var ProductCustomizer = createReactClass({
+    getInitialState: function() {
+      return {
+        color: "green",
+        size: 8,
+        sizes: window.Inventory.allSizes
+      };
+    },
+    
+    render: function() {
+      return (
+        <div className="customizer">
+          <div className="product-image">
+            <ProductImage color={this.state.color} />
+          </div>
+          <div className="selectors">
+            <SizeSelector size={this.state.size} sizes={this.state.sizes} />
+          </div>
         </div>
-        <div className="selectors">
-          <SizeSelector size={8} />
-        </div>
-      </div>
-    );
-  }
+      );
+    }
+  });
   
   ReactDOM.render(<ProductCustomizer />, document.getElementById('react-root'));
 
